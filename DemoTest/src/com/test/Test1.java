2db0e76e46df47f7b8e60e98a4458a82d9eceb49
Ameya1818 <ameyashingane0109@gmail.com>
2025-08-25 13:49:37 +0530

    test2 added

--- DemoTest/src/com/test/Test1.java
+++ DemoTest/src/com/test/Test1.java
@@ -1,9 +1,12 @@
 package com.test;
 
+import javax.jws.soap.SOAPBinding;
+
 public class Test1 {
 
 	public static void main(String[] args) {
 		
 		System.out.println("test 1>>>");
+		System.out.println("test2>>");
 	}
 }
