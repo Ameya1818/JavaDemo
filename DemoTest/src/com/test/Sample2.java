888d1d79b3540f022676134e38fe6952f5c3f11a
Ameya1818 <ameyashingane0109@gmail.com>
2025-08-25 14:53:11 +0530

    First commit on FB-01

---
+++ DemoTest/src/com/test/Sample2.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import java.util.*;
+
+public class Sample2 {
+
+	
+public static void main(String[] args) {
+	List<Integer>list=Arrays.asList(10,204,506,06);
+	System.out.println(list);
+}
+}
